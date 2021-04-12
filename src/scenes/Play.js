e96b10147ba4a57a7148bfe71d2e6dac88fdd44d
Jason Lee <jlee668@ucsc.edu>
2021-04-11 21:49:44 -0700

    second commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -59,6 +59,23 @@ class Play extends Phaser.Scene{
             }),
             frameRate: 30
         });
+
+        this.p1Score = 0;
+        // display score
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right', 
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score,
+            scoreConfig);
     }
 
     update(){
@@ -108,6 +125,8 @@ class Play extends Phaser.Scene{
             ship.reset();
             ship.alpha = 1;
             boom.destroy();
-        })
+        });
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }
